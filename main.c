68cda13f48b00f6d4ea18609c88f22ccaaff1a00
StevenMario <stevenkoumar@gmail.com>
2024-09-08 20:06:22 +0300

    Creation du structure

--- main.c
+++ main.c
@@ -6,23 +6,54 @@
 /*   By: mrambelo <mrambelo@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 10:07:37 by irabesan          #+#    #+#             */
-/*   Updated: 2024/09/05 10:02:14 by mrambelo         ###   ########.fr       */
+/*   Updated: 2024/09/08 19:12:20 by mrambelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int stock_char(t_data *data, char *input,int *i)
+{
+	int j;
+	j = 0;
+
+	while (input[*i])
+	{
+		while (input[i] == ' ')
+			i++;
+		j = i;
+	}
+}
+
+int init_data(t_data *data, char *input)
+{
+	int i;
+
+	i = 0;
+	while (input[i])
+	{
+		while (input[i] == ' ')
+			i++;
+		while (input[i] != ' ' && input[i] != '|')
+			i++;
+		stock_char(data, input, &i);
+	}
+	return 0;
+}
+
 int main(int argc,char **argv)
 {
-	(void)argv;
 	char *input;
+	t_data *data;
+
+	(void)argv;
 	if (argc > 1)
 		printf("[Error].Run without argument !\n");
 	while (1)
 	{
 		input = readline("Minishell_madafucka: ");
  		printf("%s \n",input);
+		init_data(data,input);
 	}
-    
     return (0);
 }
--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: mrambelo <mrambelo@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 09:33:52 by mrambelo          #+#    #+#             */
-/*   Updated: 2024/09/05 09:48:38 by mrambelo         ###   ########.fr       */
+/*   Updated: 2024/09/08 18:48:29 by mrambelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,26 @@
 # include <stdlib.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+
+# define CMD 0
+# define ARG 1
+# define PIPE 2
+# define INPUT 3
+# define HEREDOC 4
+# define TRUNC 5
+# define APPEND 6
+
+typedef struct s_token
+{
+	char    *content;
+	int     type;
+	struct s_token *prev;
+	struct s_token *next;
+}   t_token;
+
+typedef struct s_data
+{
+	t_token *token;
+}   t_data;
+
 #endif
