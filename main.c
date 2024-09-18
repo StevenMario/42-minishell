3913b550a235f24d584af4e71f3d025ae7556fc8
mrambelo <mrambelo@student.42antananarivo.mg>
2024-09-18 11:08:23 +0300

    Check error

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mrambelo <mrambelo@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/20 10:07:37 by irabesan          #+#    #+#             */
-/*   Updated: 2024/09/16 18:55:00 by mrambelo         ###   ########.fr       */
+/*   Updated: 2024/09/18 09:04:34 by mrambelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,18 @@ int init_data(t_data *data, char *input)
 	char **line;
 	line = ft_split(input, '|');
 	init_token(data,line);
+	while (data->token)
+	{
+		printf("data->token->content = %s  type == %d\n",data->token->content,data->token->type);
+		data->token = data->token->next;
+	}
 	return 0;
 }
+
+// void check_error(char *input)
+// {
+	
+// }
 
 int main(int argc,char **argv)
 {
@@ -27,12 +37,17 @@ int main(int argc,char **argv)
 
 	(void)argv;
 	data = malloc(sizeof(t_data));
+	if (!data)
+		return (1);
 	data->token = malloc(sizeof(t_token));
+	if (!data->token)
+		return (1);
 	if (argc > 1)
 		printf("[Error].Run without argument !\n");
 	while (1)
 	{
 		input = readline("minishell$: ");
+		//check_error(input);
 		init_data(data,input);
 	}
     return (0);
