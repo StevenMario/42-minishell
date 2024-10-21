e8400ae794eb13f191a0d117d36fe4fcb1e76ff9
StevenMario <stevenkoumar@gmail.com>
2024-10-21 22:13:39 +0300

    Initialisation dui structure t_cmd

--- init_cmd.c
+++ init_cmd.c
@@ -6,7 +6,7 @@
 /*   By: mrambelo <mrambelo@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 10:32:35 by mrambelo          #+#    #+#             */
-/*   Updated: 2024/10/21 12:26:37 by mrambelo         ###   ########.fr       */
+/*   Updated: 2024/10/21 22:13:30 by mrambelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,27 +28,26 @@ int get_nb_arg(t_token *token)
 
 void	get_cmd(t_token *token, t_cmd *cmd)
 {
-	int	i;
+	static int	i;
 
-	i = 0;
 	if (token && token->type != PIPE)
 	{
 		if (token->type == ARG || token->type == FILE || token->type == DELIMITER)
 		{
 			if (!cmd->arg)
 			{
-				printf("token	: [%s]\n", token->content);
-				printf("nb_arg = %d\n",get_nb_arg(token));
 				cmd->arg = (char **)malloc(sizeof(char *) * get_nb_arg(token) + 1);
 				if (!cmd->arg)
 					return ;
+				cmd->arg[get_nb_arg(token)] = NULL;
 			}
-			cmd->arg[i++] = ft_strdup(token->content);
+			cmd->arg[i] = ft_strdup(token->content);
+			i++;
 		}
 		else
 		{
-			// printf("token add	: [%s]\n", token->content);
 			cmd->cmd = ft_strdup(token->content);
+			i = 0;
 		}
 	}
 }
@@ -72,8 +71,8 @@ t_cmd	*ft_initcmd(void)
 
 
 	new_cmd = malloc(sizeof(t_cmd));
-	new_cmd->cmd = NULL;
 	new_cmd->arg = NULL;
+	new_cmd->cmd = NULL;
 	new_cmd->env = NULL;
 	new_cmd->next = NULL;
 	return (new_cmd);
@@ -83,29 +82,38 @@ void new_cmd(t_token *token,t_cmd **cmd)
 {
 	t_cmd	*new_cmd;
 
+	new_cmd = ft_initcmd();
+	if (!new_cmd)
+		return ;
 	while (token)
 	{
-		new_cmd = ft_initcmd();
-		if (!new_cmd)
-			return ;
 		get_cmd(token, new_cmd);
-		if(token->type == PIPE)
+		if(token->type == PIPE 
+			|| !token->next )
+		{
 			ft_add_back_cmd(cmd, new_cmd);
+			new_cmd = ft_initcmd();
+			if (!new_cmd)
+				return ;
+		}
+		// if (token->type == HEREDOC)
+		// {
+		// 	new_cmd = ft_initcmd();
+		// 	if (!new_cmd)
+		// 		return ;
+		// }
 		token = token->next;
 	}
-	// if ((*cmd) && (*cmd)->cmd)
-	// 	printf("cmd->cmd = %s\n",(*cmd)->cmd);
-	// else
-	// 	printf("Tsis zavatra\n");
-	// int i;
-	// while ((*cmd))
-	// {
-	// 	i = -1;
-	// 	if ((*cmd) && (*cmd)->cmd)
-	// 		printf("cmd->cmd = %s\n",(*cmd)->cmd);
-	// 	while ((*cmd)->arg[++i])
-	// 		printf("arg = %s\n",(*cmd)->arg[i]);
-	// 	(*cmd) = (*cmd)->next;
-	// }
+	int i;
+	while ((*cmd))
+	{
+		i = -1;
+		printf("------------------------\n");
+		if ((*cmd) && (*cmd)->cmd)
+			printf("cmd->cmd = %s\n",(*cmd)->cmd);
+		while ((*cmd)->arg[++i])
+			printf("arg = %s\n",(*cmd)->arg[i]);
+		(*cmd) = (*cmd)->next;
+	}
 
 }
