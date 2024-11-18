aa9a0ce94bba4ece58ce036c5f58e5486ebd9949
Iarantsoa Evan Rabesandratana <irabesan@c1r5s12.42antananarivo.mg>
2024-11-18 16:18:39 +0300

    exec_pipe

--- execution/exec.c
+++ execution/exec.c
@@ -6,30 +6,12 @@
 /*   By: irabesan <irabesan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 12:27:09 by irabesan          #+#    #+#             */
-/*   Updated: 2024/11/18 08:59:59 by irabesan         ###   ########.fr       */
+/*   Updated: 2024/11/18 16:17:53 by irabesan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exec.h"
 
-// int	ft_exec(t_cmd *cmd, t_env *env)
-// {
-// 	pid_t	pid;
-// 	// int	status;
-// 	// int	cmd_process;
-
-// 	pid = fork();
-// 	// cmd_process = check_cmd_process(cmd);
-// 	// if (pid == 0 && cmd_process == 1)
-// 	// 	exit (0);
-// 	// printf("pid = %d\n",pid);
-// 	if (pid == 0)
-// 		exec_extern_cmd(env, cmd);
-// 	else 
-// 		return (1);
-// 	return (0);
-	
-// }
 int	exec_simple_cmd(t_data *mish, t_cmd *cmd, t_env *env)
 {
 	if (ft_is_builtin(cmd) == 1)
@@ -48,4 +30,12 @@ int	exec_simple_cmd(t_data *mish, t_cmd *cmd, t_env *env)
 		wait(0);
 	}
 	return (0);
+}
+
+int	exec_mltp_cmd(t_data *mish, t_cmd *cmd)
+{
+	int	fd[2];
+
+	pipe(fd);
+	
 }
