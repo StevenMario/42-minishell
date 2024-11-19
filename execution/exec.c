34aa21199cdebfa66416c25ccab698a7c5bbb3c1
Iarantsoa Evan <iarantsoaevan@gmail.com>
2024-11-19 21:46:22 +0300

    exec

--- execution/exec.c
+++ execution/exec.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: irabesan <irabesan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: iarantsoa <iarantsoa@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 12:27:09 by irabesan          #+#    #+#             */
-/*   Updated: 2024/11/18 16:17:53 by irabesan         ###   ########.fr       */
+/*   Updated: 2024/11/19 21:45:31 by iarantsoa        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,10 +32,17 @@ int	exec_simple_cmd(t_data *mish, t_cmd *cmd, t_env *env)
 	return (0);
 }
 
-int	exec_mltp_cmd(t_data *mish, t_cmd *cmd)
+int	ft_fork_cmd(t_data *mish, t_cmd *cmd)
 {
 	int	fd[2];
 
 	pipe(fd);
+	cmd->pid = fork();
+	if (cmd->pid == -1)
+		perror("fork");
+	if (cmd->pid == 0)
+	{
+		
+	}
 	
 }
