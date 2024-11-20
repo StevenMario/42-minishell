74ef248936b22362d44d3fbaebc7a956c27b0093
mrambelo <mrambelo@student.42antananarivo.mg>
2024-11-20 08:45:53 +0300

    Aplication de la norminette

--- builtins/builtins.h
+++ builtins/builtins.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtins.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: irabesan <irabesan@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mrambelo <mrambelo@student.42antananari    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 14:21:53 by irabesan          #+#    #+#             */
-/*   Updated: 2024/11/18 08:46:38 by irabesan         ###   ########.fr       */
+/*   Updated: 2024/11/20 08:45:44 by mrambelo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,8 @@ int		ft_unset(t_cmd *cmd, t_env *env);
 int		ft_pwd(t_env *env);
 int		ft_env(t_cmd *cmd, t_env *env);
 void	printf_t_env(t_env *env);
-int	ft_echo(char **echo);
-int	ft_export(t_cmd *cmd, t_env *env);
+int		ft_echo(char **echo);
+int		ft_export(t_cmd *cmd, t_env *env);
 void	ft_print_dx(t_env *env);
 void	ft_lstadd_back_env(t_env **f_elem, t_env *n_elem);
 t_env	*fill_env_in_t_env(char **envp);
