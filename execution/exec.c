f395c443ea9391114a697ccab96d1990566caf43
Iarantsoa Evan <iarantsoaevan@gmail.com>
2024-11-20 13:43:02 +0300

    do_exit_status

--- execution/exec.c
+++ execution/exec.c
@@ -6,7 +6,7 @@
 /*   By: iarantsoa <iarantsoa@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 12:27:09 by irabesan          #+#    #+#             */
-/*   Updated: 2024/11/19 21:45:31 by iarantsoa        ###   ########.fr       */
+/*   Updated: 2024/11/20 13:17:00 by iarantsoa        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,17 +32,16 @@ int	exec_simple_cmd(t_data *mish, t_cmd *cmd, t_env *env)
 	return (0);
 }
 
-int	ft_fork_cmd(t_data *mish, t_cmd *cmd)
+int piping_cmd(t_data *mish)
 {
-	int	fd[2];
+	t_cmd *cmd;
+	int count;
 
-	pipe(fd);
-	cmd->pid = fork();
-	if (cmd->pid == -1)
-		perror("fork");
-	if (cmd->pid == 0)
+    count = ft_count_cmd(cmd);
+	cmd = mish->cmd;
+	if (count == 1)
 	{
-		
+		exec_simple_cmd(mish, cmd, mish->env);
+        get_exit_status();
 	}
-	
 }
--- execution/exec_utils.c
+++ execution/exec_utils.c
@@ -6,13 +6,36 @@
 /*   By: iarantsoa <iarantsoa@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/19 20:11:23 by iarantsoa         #+#    #+#             */
-/*   Updated: 2024/11/19 20:52:21 by iarantsoa        ###   ########.fr       */
+/*   Updated: 2024/11/20 13:41:09 by iarantsoa        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "exec.h"
 
-void	ft_dup_cmd(t_cmd *cmd)
+void dup_std(int in, int out)
 {
-	
+	in = dup(STDIN_FILENO);
+	out = dup(STDOUT_FILENO);
+}
+
+int	ft_count_cmd(t_cmd *cmd)
+{
+	int    count;
+
+    count = 0;
+    while (cmd)
+    {
+        i++;
+        cmd = cmd->next;
+    }
+    return (count);
+}
+
+void get_exit_status(t_data *mish)
+{
+	int wstatus;
+
+	wait(&wstatus);
+	if (WIFEXITED(wstatus))
+		mish->exit_status = WEXITSTATUS(wstatus);
 }
