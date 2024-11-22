513fafd94066551cc99dd759e4e4a01ab54c490e
StevenMario <stevenkoumar@gmail.com>
2024-11-23 01:27:18 +0300

    Creation du gestion des redires

---
+++ execution/redire_handler.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redire_handler.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrambelo <mrambelo@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/23 01:23:17 by mrambelo          #+#    #+#             */
+/*   Updated: 2024/11/23 01:26:21 by mrambelo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "exec.h"
+
+int infile_handler(t_cmd *cmd)
+{
+    t_file *infile;
+
+    infile = cmd->infile;
+    if (infile)
+    {
+        while (infile)
+        {
+            
+        }
+    }
+}
